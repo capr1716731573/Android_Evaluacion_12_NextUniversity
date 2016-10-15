89aad91295f839fc5b61cd4c24ffad901d54b1aa
capr1716731573 <capr.tics@gmail.com>
2016-10-15 07:12:36 -0500

    Se realizo el cambio de nombre en el boton Login de Facebook

--- app/src/main/java/com/carpullre/facebook_android/MainActivity.java
+++ app/src/main/java/com/carpullre/facebook_android/MainActivity.java
@@ -27,7 +27,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     private CallbackManager facebookCallBackManager;
-    private LoginButton facebookLoginButton;
+    private LoginButton LoginButton;
 
     private AdView banner_AdView;
     /**
@@ -50,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        facebookLoginButton = (LoginButton) findViewById(R.id.btn_Login_Facebook);
+        LoginButton = (LoginButton) findViewById(R.id.btn_Login_Facebook);
         //Tratamiento del Resultado al quere registrar en la app con el usuario de facebook
-        facebookLoginButton.registerCallback(facebookCallBackManager, new FacebookCallback<LoginResult>() {
+        LoginButton.registerCallback(facebookCallBackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Toast.makeText(MainActivity.this, "App Facebook - Inicio de Sesion con Exito!!!", Toast.LENGTH_SHORT).show();
